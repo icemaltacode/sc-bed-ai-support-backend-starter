248c98c83c2afb410581e1f148a0aa8637561835
Keith Vassallo <keith@icemalta.com>
2025-07-25 08:29:01 +0100

    Removed route imports from app.js

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 import express from "express";
 import cors from "cors";
-import { loadFactsheet } from "./utils/pdf.js";
-import chatRouter from "./routes/chat.js";
-import suggestionRouter from "./routes/suggestions.js";
-
-
-await loadFactsheet();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/chat", chatRouter);
-app.use("/suggestions", suggestionRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
